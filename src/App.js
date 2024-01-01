8944df8f3b0685ce96a699f8a79eeacfcab5fe4f
x-farzan <farzanhassan245@gmail.com>
2024-01-01 20:45:17 +0500

    Header, Balance and expense components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
