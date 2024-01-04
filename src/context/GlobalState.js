11cc9eeaf8d39becdfdb129f11762c977676aed7
Farzan-TekHQS <farzan.hassan@tekhqs.com>
2024-01-04 16:48:53 +0500

    Action applied | Add Transaction applied

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -25,9 +25,20 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
     >
       {children}
     </GlobalContext.Provider>
